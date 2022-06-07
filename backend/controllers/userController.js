8f453543202a94b0862aad2b5755d27a1ea3532c
Rhinolamer <rhinolamer@gmail.com>
2022-06-08 00:42:14 +0300

    checking

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,12 +74,7 @@ export const loginUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/me
 // @access  Private
 export const getMe = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user._id);
-  res.status(200).json({
-    id: _id,
-    name,
-    email,
-  });
+  res.status(200).json(req.user);
 });
 
 //Generate JWT token
--- frontend/src/screens/Dashboard.jsx
+++ frontend/src/screens/Dashboard.jsx
@@ -1,6 +1,16 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 function Dashboard() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
   return <div>dashboard</div>;
 }
 
